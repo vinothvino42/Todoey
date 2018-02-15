1a15b0f43bd33fc1837e1f189e06adcffc09f343
vinoth <vinothvino42@gmail.com>
2018-02-16 01:32:48 +0530

    Updating Data with Realm Database

--- Todoey/Controller/TodoListVC.swift
+++ Todoey/Controller/TodoListVC.swift
@@ -47,8 +47,18 @@ class TodoListVC: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-//        todoItems[indexPath.row].done = !todoItems[indexPath.row].done
-//        saveItems()
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving done status \(error)")
+            }
+        }
+        
+        tableView.reloadData()
+
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
